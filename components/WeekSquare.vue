c53487b882401340ce1126c6dba244e71ee49904
PannH <pannh2609@gmail.com>
2023-12-15 11:55:33 +0100

    display the user age at the week

--- components/WeekSquare.vue
+++ components/WeekSquare.vue
@@ -1,13 +1,17 @@
 <script setup lang="ts">
-defineProps<{
+import constants from '../constants';
+
+const props = defineProps<{
    isFilled: boolean;
    isCurrent: boolean;
    index: number;
 }>();
+
+const age = Math.floor((props.index + 1) / constants.WEEKS_PER_YEAR);
 </script>
 
 <template>
-   <div class="tooltip" :data-tip="`#${index + 1} ${isCurrent ? '- Today' : ''}`">
+   <div class="tooltip" :data-tip="`#${index + 1} (${age} year old) ${isCurrent ? '- Today' : ''}`">
       <div :class="`border rounded w-4 aspect-square ${isCurrent ? 'bg-secondary border-secondary' : isFilled ? 'bg-primary border-primary' : 'border-gray-500'}`"></div>
    </div>
 </template>
